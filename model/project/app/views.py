627bf7a8a661717d350abde3b92d66cd31cf36bb
mantashasheikh <mantashasheikh95844@gmail.com>
2026-07-07 09:16:35 +0530

    orm multiple object query

--- model/project/app/views.py
+++ model/project/app/views.py
@@ -3,27 +3,47 @@ from .models import Student
 
 # Create your views here.
 def landing(req):
-    Student.objects.create(name="Mantasha",email="man@gmail.com",city="Bhopal")
-    Student.objects.bulk_create([Student(name="Arav",email="arav@gmail.com",city="Delhi"),
-                                 Student(name="Mohini",email="mohi@gmail.com",city="Mumbai"),
-                                 Student(name="seema",email="seema@gmail.com",city="jabalpur")])
+    # Student.objects.create(name="Mantasha",email="man@gmail.com",city="Bhopal")
+    # Student.objects.bulk_create([Student(name="Arav",email="arav@gmail.com",city="Delhi"),
+    #                              Student(name="gaurav",email="g@gmail.com",city="Mumbai"),
+    #                              Student(name="meena",email="meena@gmail.com",city="jaipur")])
     
-    data = Student.objects.earliest('name')
-    print('Earliest : ', data)
-    data = Student.objects.latest('name')
-    print('Latest : ', data)
+    # data = Student.objects.earliest('name')
+    # print('Earliest : ', data)
+    # data = Student.objects.latest('name')
+    # print('Latest : ', data)
     
-    data = Student.objects.earliest('city')
-    print('Earliest_city : ', data)
-    data = Student.objects.latest('city')
-    print('Latest_city : ', data)
+    # data = Student.objects.earliest('city')
+    # print('Earliest_city : ', data)
+    # data = Student.objects.latest('city')
+    # print('Latest_city : ', data)
     
-    data = Student.objects.first()
-    print('first : ',data)
-    data = Student.objects.last('name')
-    print('lastest : ',data)
-    data = Student.objects.get('name':"reena")
-    print('lastest : ',data)
+    # data = Student.objects.first()
+    # print('first : ',data)
+    # data = Student.objects.last('name')
+    # print('lastest : ',data)
+    # data = Student.objects.get()
+    # print('lastest : ',data)
+    
+    # data = Student.objects.filter(name = "Arav")
+    # print('Earliest : ', data)
+    
+    # data = Student.objects.all()
+    # print('Earliest : ', data)
+    
+    # data = Student.objects.exclude(name = "Arav")
+    # data = Student.objects.values()
+    # data = Student.objects.values_list()
+    # data = Student.objects.values('name' , 'city)
+    # data = Student.objects.values_list('name' , 'city)
+    print(data)
+    
+    
+    
+    
+    
+    
+    
     
     
     
