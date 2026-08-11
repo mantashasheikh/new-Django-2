833cce05473f52e7d798229194da66bc1159d77b
mantashasheikh <mantashasheikh95844@gmail.com>
2026-08-11 10:11:47 +0530

    add rollno and student

--- modelrelation/onetoone/project/app/urls.py
+++ modelrelation/onetoone/project/app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing_page, name='landing'),
+    path('', views.landing, name='landing'),
     path('add-roll-number/', views.roll_number_view, name='roll_number_view'),
     path('add-student/', views.student_view, name='student_view'),
 ]
