14d9bcb4a84a5a871b38f569f15e261fb0892a32
mantashasheikh <mantashasheikh95844@gmail.com>
2026-08-11 09:54:26 +0530

    add roll number and student

--- modelrelation/onetoone/project/app/urls.py
+++ modelrelation/onetoone/project/app/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.landing, name="home"), 
-    path('addRollno' , views.addRollno , name='addRollno'),
+    path('', views.landing_page, name='landing'),
+    path('add-roll-number/', views.roll_number_view, name='roll_number_view'),
+    path('add-student/', views.student_view, name='student_view'),
 ]
--- modelrelation/onetoone/project/app/views.py
+++ modelrelation/onetoone/project/app/views.py
@@ -1,9 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import RollNo, Student
 
-# Create your views here.
+
 def landing(req):
-    return render(req,'landing.html')
+    return render(req, 'landing.html')
 
 
-def  addRollno(req):
-    return redirect()
+def roll_number_view(req):
+
+    if req.method == "POST":
+
+        roll_no = req.POST.get("roll_no")
+
+        if roll_no:
+            RollNo.objects.create(
+                roll_no=roll_no
+            )
+
+            return redirect('student_view')
+
+    return render(req, 'landing.html', {'show_roll_form': True})
+
+
+def student_view(req):
+
+    roll_numbers = RollNo.objects.all()
+
+    if req.method == "POST":
+
+        Student.objects.create(
+            name=req.POST.get("name"),
+            email=req.POST.get("email"),
+            city=req.POST.get("city"),
+            address=req.POST.get("address"),
+            contact=req.POST.get("contact"),
+            roll_id=req.POST.get("roll")
+        )
+
+        return redirect('landing')
+
+    return render(req, 'landing.html', {'show_student_form': True, 'roll_numbers': roll_numbers})
