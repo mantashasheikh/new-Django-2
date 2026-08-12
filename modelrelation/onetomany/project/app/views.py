ce4509a5a878bec0af0ce2f7ec1e6d327b285754
mantashasheikh <mantashasheikh95844@gmail.com>
2026-08-12 16:15:42 +0530

    add department and student one to many relation

--- modelrelation/onetomany/project/app/views.py
+++ modelrelation/onetomany/project/app/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Department, Student
 
-# Create your views here.
+
+
+def landing(req):
+    return render(req, 'landing.html')
+
+
+
+def department_view(req):
+
+    if req.method == 'POST':
+
+        d_name = req.POST.get('d_name')
+        d_disc = req.POST.get('d_disc')
+
+        Department.objects.create(
+            d_name=d_name,
+            d_disc=d_disc
+        )
+
+        return redirect('landing')
+
+    return render(req, 'landing.html', {'show_department': True})
+
+
+
+def student_view(req):
+
+    departments = Department.objects.all()
+
+    if req.method == 'POST':
+
+        s_name = req.POST.get('s_name')
+        s_city = req.POST.get('s_city')
+        dep_id = req.POST.get('dep_name')
+
+        department = Department.objects.get(
+            id=dep_id
+        )
+
+        Student.objects.create(
+            s_name=s_name,
+            s_city=s_city,
+            dep_name=department
+        )
+
+        return redirect('landing')
+
+    return render(req, 'landing.html',{'show_student': True, 'departments': departments})
